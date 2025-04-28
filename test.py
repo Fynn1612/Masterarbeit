7947a42f1a7290de8b4491ef2a5af722b3870495
Fynn1612 <f.schueder95@gmx.de>
2025-04-28 12:11:22 +0200

    update workspace settings and add new notebook with numpy examples

--- test.py
+++ test.py
@@ -1,2 +1,22 @@
-print('hello World')
+import numpy as np
+
+
+print("hello World")
+
+
+def add(x, y):
+    return x + y
+
+
+def devide(x, y):
+    return x / y
+
+
+def subtract(x, y):
+    return x - y
+
+
+np.random.seed(0)
+a = np.random.randint(1, 10, size=(3, 3))
+print(a)
 
