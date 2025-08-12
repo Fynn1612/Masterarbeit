3233273400245b5114fbfc6ac28f705c2dbba2d7
Fynn1612 <f.schueder95@gmx.de>
2025-08-12 11:12:38 +0200

    Remove debug logs and wandb run file; add compiled bytecode for data preparation module.

--- data_prep.py
+++ data_prep.py
@@ -1,13 +1,21 @@
+# function that take the dataframes path as input and returns the train_set, val_set, test_set
 # fuction that takes as input train_set, val_set, test_set as dataframes and a list of categorial variables
 # determine the frequency of each category in the dataframes
 # return train_set, val_set, test_set dataframes with the frequency of each category
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+import os
+import pandas as pd
+
+# function that transforms the categorical variables in the dataframes to their frequency
 # delete the original columns from the dataframes
 def cat_transform(train_df, val_df, test_df, cat_vars):
     """
-    train_set, val_set, test_set
-    cat_ vars: list categorical variables to transform
-    return: original dataframe with the frequency of each category 
-    and removed original columns
+    @param train_set, val_set, test_set    dataframes with the data
+    @param cat_ vars                       list categorical variables to transform
+    
+    @return train_df, val_df, test_df      train_df, val_df, test_df: original dataframes with the frequency of each category and removed original columns
     """
     # loop through each categorical variable
     if not isinstance(cat_vars, list):
@@ -23,3 +31,59 @@ def cat_transform(train_df, val_df, test_df, cat_vars):
             # drop the cat_var column
             df.drop(columns=[cat_var], inplace=True)
     return train_df, val_df, test_df
+
+
+# function that loads, transforms and splits the data it into train, val and test sets, and returns the feature names
+def load_tranform_and_split_data(target, split_ratio=(0.6, 0.2, 0.2)):
+    """
+    @param target                                                                 target variable to predict
+    @param split_ratio                                                            tuple with the split ratio for train, val and test sets, default is (0.6, 0.2, 0.2)
+    
+    @return X_train, X_val, X_test, y_train, y_val, y_test, feature_names         splitted and transformed dataframes with the features and target variable, and the feature names
+    """
+    
+    # check the computer name to determine the path to the data
+    if os.environ['COMPUTERNAME'] == 'FYNN':            # name of surface PC
+        path = r"C:\Users\Surface\Masterarbeit\data\Produktionsdaten\WZ_2_Feature_Engineered_Fynn6.xlsx"
+    elif os.environ['COMPUTERNAME'] == 'DEIN_DESKTOPNAME':  # replace with your desktop name
+        path = r"C:\Users\test\Masterarbeit\data\WZ_2_Feature_Engineered_Fynn6.xlsx"
+        
+    else:
+        raise ValueError("Unbekannter Computername: " + os.environ['COMPUTERNAME'])
+    
+    #load the data from the excel file
+    df = pd.read_excel(path)
+    
+    # get the numerical features
+    data_num = df.drop(target, axis = 1, inplace=False)
+    # get the target values
+    data_labels = df[target].to_numpy()
+
+    #calculate the validation size relative to the remaining data after test split
+    test_size = split_ratio[2]
+    val_size = split_ratio[1] / (1 - test_size) 
+    # split the data into training, validation and test sets
+    # 60% training, 20%, validation, 20% test
+    X_temp, X_test_prep, y_temp, y_test = train_test_split(data_num, data_labels, test_size= test_size, random_state=42)
+    X_train_prep, X_val_prep, y_train, y_val = train_test_split(X_temp, y_temp, test_size= val_size, random_state=42)
+
+    # use custom function "cat_transform" to map the categorical features with their frequencies
+    X_train_prep, X_val_prep, X_test_prep = cat_transform(X_train_prep, X_val_prep, X_test_prep, ['BT_NR', 'STP_NR'])
+    # print(X_train_prep.columns)
+
+    # pipeline for preprocessing the data
+    # Standard Scaler for distribution with 0 mean and 1 std., normal distributed data
+    data_pipeline = Pipeline([
+        ('std_scaler', StandardScaler())
+    ])
+
+    # get the feature names after preprocessing for the feature importance
+    feature_names = X_train_prep.columns
+
+    # fit the pipeline to the data and transform it
+    X_train = data_pipeline.fit_transform(X_train_prep)
+    X_val = data_pipeline.transform(X_val_prep)
+    X_test = data_pipeline.transform(X_test_prep)
+    
+    return X_train, X_val, X_test, y_train, y_val, y_test, feature_names
+
